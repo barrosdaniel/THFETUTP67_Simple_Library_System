77e1d1a972ff0124e27868b000c489a7a9f229d8
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-08-20 23:23:25 +0800

    feature: Add calculateFines() method to the Library class

--- Library.js
+++ Library.js
@@ -2,6 +2,7 @@ class Library {
   constructor() {
     this.books = [];
     this.patrons = [];
+    this.dailyFine = 0.10;
   }
 
   addBook(book) {
@@ -11,4 +12,18 @@ class Library {
   addPatron(patron) {
     this.patrons.push(patron);
   }
+
+  chargeFines() {
+    const now = new Date();
+
+    const latePatrons = this.patrons.filter(patron =>
+      (patron.currentBook !== null && now > patron.currentBook.dueDate)
+    )
+
+    for (let patron of latePatrons) {
+      const dateDiff = new Date(now - patron.currentBook.dueDate);
+      const daysLate = dateDiff.getDate();
+      patron.balance += daysLate * this.dailyFine;
+    }
+  };
 }
